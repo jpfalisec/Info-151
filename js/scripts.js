37a7d450605fe189a2c47afeb4e853815a019dff
jpfalisec <jf3359@drexel.edu>
2023-11-13 19:11:52 -0500

    finished js script burger button

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// alert("Hello Class");
+
+var mainNav = document.querySelector("#mainNav");
+
+var burgerBtn = document.querySelector("#burger-container button")
+console.log(burgerBtn);
+// console.log(mainNav);
+var canYouSeeMainNav = false;
+
+
+// console.log("Hello Class!");
+
+function showNav(){
+console.log("button clicked");
+if(canYouSeeMainNav === false){
+    mainNav.classList.add("showMainNav");
+    canYouSeeMainNav = true;
+}else{
+    mainNav.classList.remove("showMainNav");
+    canYouSeeMainNav = false;
+}
+
+
+
+}
+// add a event listener to the button
+burgerBtn.addEventListener("click", showNav);
+
+
+// will call function with the name of showNav
+// showNav();
