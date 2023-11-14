132b23bd57cf8097346aeadbe0927e57c2e182cd
jpfalisec <jf3359@drexel.edu>
2023-11-14 18:35:52 -0500

    updated mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
-var mainNav = document.querySelector("mainNav");
+// alert("Hello Class");
 
-var burgerBtn = document.querySelector("#burger-container button");
+var mainNav = document.querySelector("#mainNav");
+
+var burgerBtn = document.querySelector("#burger-container button")
 console.log(burgerBtn);
-
+// console.log(mainNav);
 var canYouSeeMainNav = false;
 
+// console.log("Hello Class!");
 
 function showNav(){
-    console.log("button clicked");
-
-    if(canYouSeeMainNav === false){
-        mainNav.classList.add("showMainNav");
-    }else{
-        mainNav.classList.remove("showMainNav");
-        canYouSeeMainNav = false;
-    }
+console.log("button clicked");
+if(canYouSeeMainNav === false){
+    mainNav.classList.add("showMainNav");
+    canYouSeeMainNav = true;
+}else{
+    mainNav.classList.remove("showMainNav");
+    canYouSeeMainNav = false;
 }
 
+
+}
+// add a event listener to the button
 burgerBtn.addEventListener("click", showNav);
