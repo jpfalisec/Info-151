a5a1e3d971f3fdaf9d2cfc77a16845ff3530505b
jpfalisec <jf3359@drexel.edu>
2023-11-30 20:17:13 -0500

    finished week 8

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
-// alert("Hello Class");
+/* -----------------
+    Navigation
+-------------------- */
 
 var mainNav = document.querySelector("#mainNav");
+var signInUpButton = document.querySelector("#sign-in-up-button");
+var burgerBtn = document.querySelector("#burger-container button");
 
-var burgerBtn = document.querySelector("#burger-container button")
-console.log(burgerBtn);
-// console.log(mainNav);
 var canYouSeeMainNav = false;
 
 
-// console.log("Hello Class!");
-
 function showNav(){
-console.log("button clicked");
 if(canYouSeeMainNav === false){
+    console.log("show menu");
     mainNav.classList.add("showMainNav");
+
+    signInUpButton.classList.add("showSignUp");
     canYouSeeMainNav = true;
 }else{
+    console.log("hide menu");
     mainNav.classList.remove("showMainNav");
+
+    signInUpButton.classList.remove("showSignUp");
     canYouSeeMainNav = false;
 }
 
@@ -29,3 +33,25 @@ burgerBtn.addEventListener("click", showNav);
 
 // will call function with the name of showNav
 // showNav();
+
+/* -----------------
+    Sign-Up
+-------------------- */
+var signUpBtn = document.querySelector("#sign-up-btn");
+var signUpContainer = document.querySelector("#signup-container");
+var closeSignUpModalBtn = document.querySelector("#close-sign-up-modal-btn");
+
+function showSignUpModal(){
+    signUpContainer.classList.add("show-signUp-container");
+    // hide the navigation on close
+    mainNav.classList.remove("showMainNav");
+    // hide the sign in/up buttons
+    signInUpButton.classList.remove("showSignUp");
+}
+
+function hideSignUpModal(){
+    signUpContainer.classList.remove("show-signUp-container");
+}
+
+signUpBtn.addEventListener("click", showSignUpModal);
+closeSignUpModalBtn.addEventListener("click", hideSignUpModal);
